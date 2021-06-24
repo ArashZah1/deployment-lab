a1f85f7d1a997772557e30e9a0748d582b8ff9c9
Arash Zaheri <arashzaheri@gmail.com>
2021-06-24 16:57:55 -0500

    fix

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const path = require('path') //path is a package 
 
+app.use(express.static(path.join(__dirname, '/public')))
+
 app.get('/', function(req, res){
     res.sendFile( path.join(__dirname, '../aboutme.html') )
 })
