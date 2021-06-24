baef0ae1cc3accd4a7cc984de0713c08ea05071f
Arash Zaheri <arashzaheri@gmail.com>
2021-06-24 16:18:15 -0500

    more

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const path = require('path') //path is a package
 app.get('/', function(req, res){
     res.sendFile( path.join(__dirname, '../aboutme.html') )
 })
+app.use(express.static(__dirname));
+
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
