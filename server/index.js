6c4215e68d8068ac73566b4c8eebb31ab25abafd
Arash Zaheri <arashzaheri@gmail.com>
2021-06-24 14:57:09 -0500

    intial

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path') //path is a package 
 
 app.get('/', function(req, res){
-    res.sendFile( path.join(__dirname, '../index/html') )
+    res.sendFile( path.join(__dirname, '../aboutme.html') )
 })
 
 const port = process.env.PORT || 5000
